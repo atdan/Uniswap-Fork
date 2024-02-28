1e9a912d31574263c5572962cba8871d4a59b1bf
atdan <atumadann@gmail.com>
2024-02-28 12:01:36 +0100

    update index page, import Header, Feature, Security, Token Components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import { Header,
+  Footer,
+  Feature,
+  Hero,
+  Platfrom,
+  Preloader,
+  Scroll,
+  Scurity,
+  Statistics,
+  Testomonial,
+  Token } from "../components/index"
+
+import { CONTEXT} from "../context/context";
+
+
 
 const index = () => {
-  return <div>index</div>;
+  const {TOKEN_SWAP, LOAD_TOKEN, notifyError, notifySuccess,
+    setLoader, loader, connect, address, swap} = useContext(CONTEXT);
+
+  // Open Token COmponent
+  const [token_1, setToken_1] = useState();
+  const [token_2, setToken_2] = useState();
+  const [openToken, setOpenToken] = useState(false);
+
+  // Input
+  const [slippageAmount, setSlippageAmount] = useState(2);
+  const [deadline, setDeadline] = useState(10);
+  const [inputAmount, setInputAmount] = useState(undefined);
+
+  //Output
+  const [outputAmount, setOutputAmount] = useState(undefined);
+  const [transaction, setTransaction] = useState(undefined);
+  const [ratio, setRatio] = useState(undefined);
+
+  return (
+      <div>
+
+        <Preloader />
+        <Header address={address} connect={connect}/>
+        <Hero
+            setInputAmount={setInputAmount}
+            setLoader={setLoader}
+            LOAD_TOKEN={LOAD_TOKEN}
+            token_1={token_1}
+            token_2={token_2}
+            setToken_1={setToken_1}
+            setToken_2={setToken_2}
+            swap={swap}
+        />
+        <Feature />
+        <Platfrom />
+        <Statistics />
+        <Scurity />
+        <Testomonial />
+        <Footer />
+
+          {
+              !openToken && (
+                  <div className="new_loader">
+                    <Token notifyError={notifyError} notifySuccess={notifySuccess}
+                        setOpenToken={setOpenToken}
+                           LOAD_TOKEN={LOAD_TOKEN}
+                           token_1={token_1}
+                           token_2={token_2}
+                           setToken_1={setToken_1}
+                           setToken_2={setToken_2}/>
+                  </div>
+              )
+          }
+      </div>
+  )
 };
 
 export default index;
